7e7eeaed509342aa78b3b9e41ae9d65a46e27961
taihejin <taihejin2008@126.com>
2016-06-25 13:23:38 +0800

    well done

--- chapter11/t11.6.6.c
+++ chapter11/t11.6.6.c
@@ -10,60 +10,60 @@ struct msg {
 pthread_cond_t ready = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void walk(struct msg* queue) {
-  while (queue) {
-    printf("%d->", queue->id);
-    queue = queue->next;
+struct msg* queue;
+
+void walk() {
+  struct msg* q = queue;
+  while (q) {
+    printf("%d->", q->id);
+    q = q->next;
   }
   printf("\n");
 }
 
-void offer(struct msg** queue, struct msg* item) {
+void offer(struct msg* item) {
   pthread_mutex_lock(&mutex);
-  item->next = *queue;
-  *queue = item;
+  item->next = queue;
+  queue = item;
   pthread_mutex_unlock(&mutex);
   pthread_cond_signal(&ready);
 }
 
-struct msg* poll(struct msg** queue) {
+struct msg* poll() {
   struct msg* item;
   pthread_mutex_lock(&mutex);
-  while ((*queue) == NULL) {
-    printf("queue addr %lx\n", (unsigned long)(*queue));
+  while (queue == NULL) {
+    printf("queue addr %lx\n", (unsigned long)queue);
     pthread_cond_wait(&ready, &mutex);
   }
-  walk(*queue);
-  item = *queue;
-  *queue = item->next;
+  item = queue;
+  queue = item->next;
   pthread_mutex_unlock(&mutex);
   return item;
 }
 
 void* thread_task(void* arg) {
-  struct msg** queue = (struct msg**)arg;
   printf("thread task %lx\n", (unsigned long)queue);
   for (;;) {
-    struct msg* item = poll(queue);
+    struct msg* item = poll();
     printf("poll msg: id %d addr %lx\n", item->id, (unsigned long)item);
-    //free(item);
-    sleep(1);
+    free(item);
+    walk();
+    sleep(3);
   }
 }
 
 int main(int argc, char* argv[]) {
   pthread_t tid;
-  struct msg* queue = NULL;
-  printf("init queue %lx\n", (unsigned long)(&queue));
-  pthread_create(&tid, NULL, thread_task, (void*)(&queue));
+  pthread_create(&tid, NULL, thread_task, (void*)(queue));
   long count = 0;
   for (;;) {
     struct msg* item = (struct msg*)malloc(sizeof(struct msg));
     item->id = (++count % 10000);
-    offer(&queue, item);
+    offer(item);
     printf("offer msg: id %d addr %lx\n", item->id, (unsigned long)item);
-    walk(queue);
-    sleep(2);
+    walk();
+    sleep(1);
   }
   return 0;
 }
