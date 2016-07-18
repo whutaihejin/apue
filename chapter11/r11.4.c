275bc1960e405ac1ea3861e03f191f5d52ae3f00
taihejin <taihejin2008@126.com>
2016-07-18 15:19:59 +0800

    format

--- chapter11/r11.4.c
+++ chapter11/r11.4.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+
 void print_ids(const char* prefix) {
   pid_t pid = getpid();
   pthread_t tid = pthread_self();
   printf("%s pid %lu tid %lu (0x%lx)\n", prefix, (unsigned long)pid, (unsigned long)tid, (unsigned long)tid);
 }
+
 void* thread_task(void* arg) {
   print_ids("new thread: ");
   return NULL;
 }
+
 int main(int argc, char* argv[]) {
   pthread_t tid;
   int ret = pthread_create(&tid, NULL, thread_task, NULL);
