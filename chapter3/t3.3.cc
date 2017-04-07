17a71ce89d5bfc1d0208bc0c5c5f4c863799033a
taihejin <taihejin@163.com>
2017-04-07 17:26:45 +0800

    learning & fighting

--- chapter3/t3.3.cc
+++ chapter3/t3.3.cc
@@ -59,6 +59,70 @@ TEST_F(OpenTest, TestRDWR) {
 //  close(fd); 
 //}
 
+TEST_F(OpenTest, TestAppend) {
+  int fd = open(path_, O_APPEND);
+  printf(" -- fd = %d, append\n", fd);
+  close(fd);
+}
+
+TEST_F(OpenTest, TestCLOEXEC) {
+  int fd = open(path_, O_CLOEXEC);
+  printf("-- fd = %d, cloexec\n", fd);
+  close(fd);
+}
+
+TEST_F(OpenTest, TestCreate) {
+  int fd = open(path_, O_CREAT);
+  printf("-- fd = %d, create\n", fd);
+  close(fd);
+}
+
+TEST_F(OpenTest, TestDirectory) {
+  int fd = open(path_, O_DIRECTORY);
+  EXPECT_EQ(-1, fd);
+  printf("-- fd = %d, directory\n", fd);
+  close(fd);
+   
+  fd = open("/tmp/", O_DIRECTORY);
+  printf("-- fd = %d, directory\n", fd);
+  EXPECT_NE(-1, fd);
+}
+
+TEST_F(OpenTest, TestExclusive) {
+  int fd = open(path_, O_CREAT | O_EXCL);
+  printf("-- fd = %d, exclusive\n", fd);
+  EXPECT_EQ(-1, fd); 
+  close(fd);
+  fd = open("/tmp/noexist.log", O_CREAT | O_EXCL);
+  EXPECT_NE(-1, fd);
+  printf("-- fd = %d, exclusive\n", fd);
+  close(fd);
+}
+
+TEST_F(OpenTest, TestNofollow) {
+  int fd = open(path_, O_NOFOLLOW);
+  printf("-- fd = %d, no follow\n", fd);
+  EXPECT_NE(-1, fd);
+  close(fd);
+  fd = open("/lib/librt.so.1", O_NOFOLLOW);
+  printf("-- fd = %d, no follow\n", fd);
+  EXPECT_EQ(-1, fd);
+  close(fd);
+}
+
+TEST_F(OpenTest, TestSync) {
+  int fd = open(path_, O_SYNC);
+  printf("-- fd = %d, sync\n", fd);
+  EXPECT_NE(-1, fd);
+  close(fd);
+}
+
+TEST_F(OpenTest, TestTruncate) {
+  int fd = open(path_, O_TRUNC);
+  printf("-- fd = %d, truncate\n", fd);
+  EXPECT_NE(-1, fd);
+  close(fd);
+}
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
